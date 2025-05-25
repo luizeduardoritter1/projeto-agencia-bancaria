a565955b6a1bdd25a57fcf93978f6e2111ae10d6
luizeduardoritter1 <luizeduardoritter6@gmail.com>
2025-05-25 00:34:30 -0300

    Exclusão dos diretórios não essenciais

--- tipos-variaveis/src/TiposVariaveis.java
+++
@@ -1,8 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
-    }
-}
